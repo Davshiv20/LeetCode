f586a51d929dadf048840e40db22fb96d2af7240
Shivam dave <103759719+Davshiv20@users.noreply.github.com>
2024-01-14 23:59:48 +0530

    Time: 197 ms (8.88%) | Memory: 21.3 MB (26.20%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+
+class Solution {
+public:
+    bool closeStrings(std::string word1, std::string word2) {
+        if (word1.size() != word2.size()) {
+            return false;
+        }
+
+        std::set<char> s1(word1.begin(), word1.end());
+        std::set<char> s2(word2.begin(), word2.end());
+
+        if (s1 != s2) {
+            return false;
+        }
+
+        std::map<char, int> m1, m2;
+
+        for (char c : word1) {
+            m1[c]++;
+        }
+
+        for (char c : word2) {
+            m2[c]++;
+        }
+
+        std::vector<int> l1 = populateVector(m1);
+        std::vector<int> l2 = populateVector(m2);
+
+        std::sort(l1.begin(), l1.end());
+        std::sort(l2.begin(), l2.end());
+
+        return l1 == l2;
+    }
+
+private:
+    std::vector<int> populateVector(const std::map<char, int>& m) {
+        std::vector<int> result;
+        for (const auto& kv : m) {
+            result.push_back(kv.second);
+        }
+        return result;
+    }
+};
