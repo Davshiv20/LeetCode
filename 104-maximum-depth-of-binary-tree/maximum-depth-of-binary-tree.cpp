4f845cafba111376ef17633d4d4aa181fbcdc7c9
Shivam dave <103759719+Davshiv20@users.noreply.github.com>
2024-07-28 22:42:33 +0530

    Time: 3 ms (92.52%) | Memory: 17.8 MB (13.91%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -6,19 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        if(root==NULL)
-        {
+    int findH(TreeNode* root, int h) {
+        if (!root)
             return 0;
-        }
-        int h1=maxDepth(root->left);
-        int h2=maxDepth(root->right);
-        return 1+max(h1,h2);
-        
+        return 1 + max(findH(root->left, h), findH(root->right, h));
+
+    }
+    int maxDepth(TreeNode* root) {
+        int h = 0;
+
+        return findH(root, h); 
+
     }
 };
