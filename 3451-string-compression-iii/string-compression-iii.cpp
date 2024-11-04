630c3834a1542a6f2b0ebe26832de6af2aa641f9
Shivam dave <103759719+Davshiv20@users.noreply.github.com>
2024-11-04 10:52:55 +0530

    Time: 70 ms (15.38%) | Memory: 29.9 MB (42.73%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     string compressedString(string word) {
-        string comp="";
-        char currChar=word[0];
-        for(int i=1;i<word.size()+1;i++)
-        {
-      
-           int count=1;
-           while(word[i]==currChar && i<=word.size() && count<9)
-           {
-                i++;
-                count++;
-               
-           }
-           comp+=to_string(count);
-           comp+=currChar;
-           currChar=word[i];
+        string res="";
+        char ch=word[0];
+        for(int i=1;i<word.size()+1;i++){
+            int count=1;
+            while(word[i]==ch && i<=word.size() && count<9){
+
+               count++;
+               i++;
+                // ch=word[i];
+            }
+            // cout<<(count)<<endl;
+            res+=to_string(count);
+            res+=ch;
+            ch=word[i];
         }
-        return comp;
+        return res;
     }
 };
